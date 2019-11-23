c61dde3639c58a14fc55917f03a99bf0a8282223
Ruben <rubenferrao1@gmail.com>
2019-11-23 00:00:54 +0000

    Começo do Design de Produtos
    Design

--- infortec_site/frontend/views/site/index.php
+++ infortec_site/frontend/views/site/index.php
@@ -55,5 +55,18 @@ $this->title = 'My Yii Application';
         <?php
     }
     ?>
-
+        <div class="allCards">
+        <?php
+        $num = 30;
+        for($i = 0; $i<$num; $i++)
+        {?>
+            <div class="card" style="width:24%;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        <?php } ?>
+        </div>
 </div>
