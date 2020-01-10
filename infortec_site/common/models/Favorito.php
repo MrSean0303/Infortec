275a36aa207f5af45c344e80a8e551bfb3b7e29e
Ruben <rubenferrao1@gmail.com>
2020-01-10 21:51:44 +0000

    nada de especial
    ...

--- infortec_site/common/models/Favorito.php
+++ infortec_site/common/models/Favorito.php
@@ -97,7 +97,7 @@ class Favorito extends \yii\db\ActiveRecord
             $this->FazPublish("UPDATE",$myJSON);
     }
 
-    public function FazPublish($canal,$msg)
+    public function FazPublish($canal, $msg)
     {
         $server = "127.0.0.1";
         $port = 1883;
@@ -105,11 +105,11 @@ class Favorito extends \yii\db\ActiveRecord
         $password = ""; // set your password
         $client_id = "phpMQTT-publisher"; // unique!
         $mqtt = new phpMQTT($server, $port, $client_id);
-        if ($mqtt->connect(true, NULL, $username, $password))
-        {
+        if ($mqtt->connect(true, NULL, $username, $password)) {
             $mqtt->publish($canal, $msg, 0);
             $mqtt->close();
+        } else {
+            file_put_contents("debug.output", "Time out!");
         }
-        else { file_put_contents("debug.output","Time out!"); }
     }
 }
